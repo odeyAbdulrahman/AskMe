6bcb0a9ab193978d1f04739fd1a677b88edd599e
odey abdalrahman <odey.abdalrahman@dal.shj.ae>
2023-04-16 03:24:33 +0400

    consume ask me api from ask me web app

--- AskMe.APP/Pages/Index.cshtml.cs
+++ AskMe.APP/Pages/Index.cshtml.cs
@@ -1,20 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
 
 namespace AskMe.APP.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory HttpClient;
+        public IEnumerable<Question>? Data { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IHttpClientFactory httpClient)
         {
-            _logger = logger;
+            HttpClient = httpClient;
         }
 
-        public void OnGet()
+        public async void OnGet()
         {
-
+            Data = JsonSerializer.Deserialize<IEnumerable<Question>?>(await SendAsync("AskMe.API", "/api/Questions"), new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true});
         }
+        #region HttpClient Factory
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<string> SendAsync(string name, string requestUrl)
+        {
+            var httpClient = HttpClient.CreateClient(name);
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+            var response = await httpClient.SendAsync(
+             request, HttpCompletionOption.ResponseHeadersRead).
+             ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        #endregion
     }
 }
